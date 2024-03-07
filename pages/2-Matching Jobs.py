6236747d46074b99164966a5c743ba276cdc0383
FlameCerberus <96816249+FlameCerberus@users.noreply.github.com>
2024-03-07 16:29:31 +0800

    Update 2-Matching Jobs.py

--- pages/2-Matching Jobs.py
+++ pages/2-Matching Jobs.py
@@ -4,7 +4,8 @@ import streamlit as st
 import requests
 import csv
 import scrape_jobs
-from jobspy import scrape_jobs
+#from jobspy import scrape_jobs
+import jobspy
 import pandas as pd
 import google.generativeai as genai
 
